1f6ec05d683dc0a151b493ba378625c30c3d0291
Ajbots456 <107096849+Ajbots456@users.noreply.github.com>
2022-06-26 16:39:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Channel : @Moviesseriesnewchannel\n○ Support Group : Click Here<URL=https://t.me/MoviesSeries_DisscussionGroup>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Channel : @Moviesseriesnewchannel\n○ Support Group : (Click Here<URL=https://t.me/MoviesSeries_DisscussionGroup>)",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
